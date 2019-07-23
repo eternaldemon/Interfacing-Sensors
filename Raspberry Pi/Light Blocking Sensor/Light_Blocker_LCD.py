810b1ea6963a724b1b7f4cac2b3dbad28aabb039
eternaldemon <44200875+eternaldemon@users.noreply.github.com>
2019-07-23 14:32:10 +0530

    Add files via upload

---
+++ Raspberry Pi/Light Blocking Sensor/Light_Blocker_LCD.py
@@ -0,0 +1,51 @@
+import RPi.GPIO as GPIO 
+import time
+from RPLCD import CharLCD
+
+# Configure the LCD
+lcd = CharLCD(numbering_mode=GPIO.BOARD,cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
+
+OutLed = 3 
+LightInPin = 21
+GPIO.setwarnings(False)
+ 
+def setup():
+    #GPIO.setmode(GPIO.BCM) # Set GPIO as PIN Numbers 
+    GPIO.setup(OutLed, GPIO.OUT) # Set Green Led Pin mode to output
+    GPIO.setup(LightInPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pull up to high level(3.3V)
+    GPIO.add_event_detect(LightInPin, GPIO.BOTH, callback=detect, bouncetime=200)
+ 
+def Led(x):
+    if x == 0:
+        GPIO.output(OutLed, 1)
+    if x == 1:
+        GPIO.output(OutLed, 0)
+ 
+def Print(x):
+    if x == 1:
+        #print (' 14CORE | Light Interrupter')
+        #print (' --------------------------')
+        lcd.write_string("Light has been interrupted")
+        time.sleep(1)
+        lcd.clear()
+        #print (' --------------------------')
+ 
+def detect(chn):
+    Led(GPIO.input(LightInPin))
+    Print(GPIO.input(LightInPin))
+ 
+def loop():
+    while True:
+        pass
+ 
+def destroy():
+    GPIO.output(OutLed, GPIO.HIGH) # Green led off
+    GPIO.output(Rpin, GPIO.HIGH) # Red led off
+    GPIO.cleanup() # Release resource
+ 
+if __name__ == '__main__': # Set the Program start from here
+    setup()
+try:
+    loop()
+except KeyboardInterrupt: # When pressed 'Ctrl+C' child program destroy() will be executed.
+    destroy()
